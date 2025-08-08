431a2c050f8c2974cbbc41caa35d98ef30d3cbe1
Mayur Vijay Kode <mayurkode7@gmail.com>
2025-08-08 20:10:56 +0530

    Refactor MongoDB connection string to use environment variables for credentials

--- ecommerce/src/app/api/db.ts
+++ ecommerce/src/app/api/db.ts
@@ -4,8 +4,7 @@ let cachedClient: MongoClient | null = null;
 let cachedDb: Db | null = null;
 
 export async function connectToDB() {
-  const uri =
-    "mongodb+srv://mayurkode7:si4YJ1VxHSRizHuU@cluster0.ntpcqpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ntpcqpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb };
