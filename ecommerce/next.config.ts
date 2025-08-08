226e49c4bf5a7e8231703819a9aa7afb5276c590
Mayur Vijay Kode <mayurkode7@gmail.com>
2025-08-08 20:31:43 +0530

    Add environment variables for MongoDB and site URL in Next.js config; update product fetching logic

--- ecommerce/next.config.ts
+++ ecommerce/next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    MONGODB_USER: process.env.MONGODB_USER,
+    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  
 };
 
 export default nextConfig;
--- ecommerce/src/app/products/page.tsx
+++ ecommerce/src/app/products/page.tsx
@@ -1,12 +1,13 @@
 // import { products } from "@/product-data";
 import ProductsList from "@/app/ProductsList";
+import { env } from "process";
 
 export const dynamic = "force-dynamic";
 
 export default async function ProductsPage() {
 
   // const products = await fetch('http://localhost:3000/api/products');
-  const products = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/products');
+  const products = await fetch(env.NEXT_PUBLIC_SITE_URL+'/api/products');
   const productsData = await products.json();
 
   return (
